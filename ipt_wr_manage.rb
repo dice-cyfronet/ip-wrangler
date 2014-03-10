bf9c67d17e8f6fc1eb3e9e0b405a9a692d1c61ea
jm <jan.meizner@cyfronet.pl>
2014-03-10 08:48:59 +0100

    - ref

--- ipt_wr_manage.rb
+++ ipt_wr_manage.rb
@@ -3,16 +3,23 @@ require 'yaml'
 require 'sqlite3'
 require File.dirname(__FILE__) + '/ipt_wr_tools'
 
-$config = YAML.load_file('config.yml')
+#$config = YAML.load_file('config.yml')
+$db_name = nil
 $ip = nil
-$tpr = nil
-$upr = nil
+$proto = nil
+$p1 = nil
+$p2 = nil
+
 
 opts = GetoptLong.new(
     ['--help', '-h', GetoptLong::NO_ARGUMENT],
+    ['--db', '-d', GetoptLong::REQUIRED_ARGUMENT],
     ['--ip', '-i', GetoptLong::REQUIRED_ARGUMENT],
-    ['--tcp-port', '-t', GetoptLong::REQUIRED_ARGUMENT],
-    ['--udp-port', '-u', GetoptLong::REQUIRED_ARGUMENT]
+    ['--tcp', '-t', GetoptLong::NO_ARGUMENT],
+    ['--udp', '-u', GetoptLong::NO_ARGUMENT],
+    ['--port', '-p', GetoptLong::REQUIRED_ARGUMENT],
+    ['--port-min', '-m', GetoptLong::REQUIRED_ARGUMENT],
+    ['--port-max', '-x', GetoptLong::REQUIRED_ARGUMENT]
 )
 
 def add_port_r(p1,p2)
@@ -45,11 +52,14 @@ def usage
   exit 1
 end
 
-def init_database
+def init_database(db_name)
   puts 'Initializing database...'
-  $db = SQLite3::Database.new('ipt_wr.db')
-  $db.execute('CREATE TABLE IF NOT EXISTS NatRules (pubIp TEXT NOT NULL, privIp TEXT NOT NULL, pubPort INT NOT NULL, privPort INT NOT NULL, proto TEXT NOT NULL, ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, Desc TEXT)')
-  $db.execute('CREATE TABLE IF NOT EXISTS NatPorts (pubIp TEXT NOT NULL, pubPort INT NOT NULL, privIp TEXT DEFAULT NULL, privPort INT DEFAULT NULL, proto TEXT DEFAULT NULL, created TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
+  db = IptWr::Database.new db_name
+  db.create
+
+  #$db = SQLite3::Database.new('ipt_wr.db')
+  #$db.execute('CREATE TABLE IF NOT EXISTS NatRules (pubIp TEXT NOT NULL, privIp TEXT NOT NULL, pubPort INT NOT NULL, privPort INT NOT NULL, proto TEXT NOT NULL, ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, Desc TEXT)')
+  #$db.execute('CREATE TABLE IF NOT EXISTS NatPorts (pubIp TEXT NOT NULL, pubPort INT NOT NULL, privIp TEXT DEFAULT NULL, privPort INT DEFAULT NULL, proto TEXT DEFAULT NULL, created TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
 end
 
 def p_decode(proto, ps)
@@ -68,7 +78,7 @@ def p_decode(proto, ps)
       raise '[add_ip] Syntax error'
   end
 
-  IPTWr::PortRange.new proto, p[0], p[1]
+  IptWr::PortRange.new proto, p[0], p[1]
 end
 
 def add
@@ -83,24 +93,65 @@ opts.each do |opt, arg|
 ipt_wr_manage [OPTIONS] <cmd>
 -h, --help:
   Help
+-d, --db
+  Database name
 -i, --ip
   Public IP address
--t, --tcp-port x[:y]
-  TCP port, or port range
--u, --udp-port x[:y]
-  UDP port, or port ramge
+-t, --tcp
+  TCP port range
+-u, --udp
+  UDP port range
+-p, --port
+  single port (excludes -m and -x)
+-m, --port-min
+  lower port (excludes -p)
+-x, --port-max
+  upper port (excludes -p)
 Commands:
 initdb - create database and fill initial data
 list - list all ports
 add - add port / all ports in range
 del - remove port / all ports in range
       EOF
+    when '--db'
+      $db_name = arg
     when '--ip'
-      $ip = IPTWr::IPAddress.new arg
-    when '--tcp-port'
-      $tpr = p_decode IPTWr::PortRange::PROTO_TCP, arg
-    when '--udp-port'
-      $upr = p_decode IPTWr::PortRange::PROTO_UDP, arg
+      $ip = arg
+    when '--tcp'
+      unless $proto.nil?
+        puts 'Conflict between --tcp and --udp'
+        exit 3
+      end
+      $proto = IptWr::Protocol::PROTO_TCP
+    when '--udp'
+      unless $proto.nil?
+        puts 'Conflict between --tcp and --udp'
+        exit 4
+      end
+      $proto = IptWr::Protocol::PROTO_UDP
+    when '--port'
+      unless $p1.nil?
+        puts 'Conflict: --port, --port-min, --port-max'
+        exit 5
+      end
+      $p1 = arg.to_i
+      unless $p2.nil?
+        puts 'Conflict: --port, --port-min, --port-max'
+        exit 6
+      end
+      $p2 = arg.to_i
+    when '--port-min'
+      unless $p1.nil?
+        puts 'Conflict: --port, --port-min, --port-max'
+        exit 7
+      end
+      $p1 = arg.to_i
+    when '--port-max'
+      unless $p2.nil?
+        puts 'Conflict: --port, --port-min, --port-max'
+        exit 8
+      end
+      $p2 = arg.to_i
     else
       # Should not happen but...
       raise Error.new
@@ -120,9 +171,32 @@ cmd = ARGV.shift
 
 case cmd
   when 'initdb'
-    init_database
+    if $db_name.nil?
+      puts 'Database name not set'
+      usage
+      exit 9
+    end
+    init_database $db_name
   when 'add'
-
+    if $db_name.nil?
+      puts 'Database name not set'
+      usage
+      exit 9
+    end
+    if $ip.nil?
+      puts 'IP not defined'
+    end
+    if $p1.nil? or $p2.nil?
+      puts 'Ports not defined'
+      exit 11
+    end
+    if $proto.nil?
+      puts 'Protocol not defined'
+      exit 12
+    end
+    nps = IptWr::NatPorts.new
+    nps.add_range $ip, $proto, $p1, $p2
+    nps.db_save IptWr::Database.new $db_name
   else
     puts 'Unknown command'
     usage
--- ipt_wr_tools.rb
+++ ipt_wr_tools.rb
@@ -1,4 +1,46 @@
-module IPTWr
+require 'sqlite3'
+
+module IptWr
+
+  class Database
+
+    def initialize(db_name)
+      @db = nil
+      @db = SQLite3::Database.new(db_name)
+    end
+
+    def create
+      raise 'Uninitialized' if @db.nil?
+      @db.execute('CREATE TABLE IF NOT EXISTS NatPorts (pubIp TEXT NOT NULL, pubPort INT NOT NULL, privIp TEXT DEFAULT NULL, privPort INT DEFAULT NULL, proto TEXT DEFAULT NULL, created TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
+    end
+
+    def save_nat_port(np)
+      @db.execute('INSERT INTO NatPorts (pubIp, pubPort, privIp, privPort, proto) VALUES (?, ?, ?, ?, ?)',
+                 np.pub_ip.to_s, np.pub_port.to_s, np.priv_ip.to_s, np.priv_port.to_s,
+                 np.protocol.to_s)
+    end
+
+    def load_nat_ports
+      nps = NatPorts.new
+      nprs = @db.execute('SELECT pubIp, pubPort, privIp, privPort, proto FROM NatPorts')
+      nprs.each do |npr|
+        case npr[4]
+          when 'tcp'
+            proto = Protocol::PROTO_TCP
+          when 'udp'
+            proto = Protocol::PROTO_UDP
+          else
+            raise 'Unknown protocol'
+        end
+        np = NatPort.new npr[2],npr[0],npr[3],npr[1],proto
+        nps.add_nat_port np
+      end
+      nps
+    end
+
+  end
+
+
   class IPAddress
 
     attr_accessor :ip_octets
@@ -8,7 +50,7 @@ module IPTWr
         @ip_octets = nil
       elsif ip.instance_of? self.class
         @ip_octets = ip.ip_octets
-      elsif p.instance_of? String
+      elsif ip.instance_of? String
         ip_octets_s = ip.split '.'
         raise 'Illegal IP format (octets count)' if ip_octets_s.length != 4
         @ip_octets = Array.new(4) { 0 }
@@ -20,13 +62,13 @@ module IPTWr
          i = i + 1
         end
       else
-        raise 'Wrong format'
+        raise "Wrong format #{ip.class}"
       end
     end
 
     def to_s
       if @ip_octets.nil?
-        'NULL'
+        nil
       else
         "#{@ip_octets[0]}.#{@ip_octets[1]}.#{@ip_octets[2]}.#{@ip_octets[3]}"
       end
@@ -46,10 +88,10 @@ module IPTWr
     end
 
     def to_s
-      if p.nil?
-        'NULL'
+      if @port.nil?
+        nil
       else
-        "#{p}"
+        "#{@port}"
       end
     end
   end
@@ -60,27 +102,38 @@ module IPTWr
     PROTO_UDP = 1
 
     def initialize(protocol)
-      raise 'Unknown protocol' if proto < PROTO_TCP or proto > PROTO_UDP
+      raise 'Unknown protocol' if protocol < PROTO_TCP or protocol > PROTO_UDP
       @protocol = protocol
     end
-  end
 
-  class PortRange
-    attr_accessor :proto, :b, :e
-
-    def each
-      b_port = @b.port
-      e_port = @e.port
-      b_port.step(e_port, 1) { |p| yield p }
-    end
-
-    def initialize(proto, b, e)
-      raise 'Ports order mismatch' if b > e
-      @proto = Protocol.new proto
-      @b = Port.new b
-      @e = Port.new e
+    def to_s
+      case @protocol
+        when PROTO_TCP
+          'tcp'
+        when PROTO_UDP
+          'udp'
+        else
+          raise 'Unknown protocol'
+      end
     end
   end
+
+  #class PortRange
+  #  attr_accessor :proto, :b, :e
+
+  #  def each
+  #    b_port = @b.port
+  #    e_port = @e.port
+  #    b_port.step(e_port, 1) { |p| yield p }
+  #  end
+
+  #  def initialize(proto, b, e)
+  #    raise 'Ports order mismatch' if b > e
+  #    @proto = Protocol.new proto
+  #    @b = Port.new b
+  #    @e = Port.new e
+  #  end
+  # end
 
   class NatPort
     attr_accessor :priv_ip, :pub_ip, :priv_port, :pub_port, :protocol
@@ -91,6 +144,10 @@ module IPTWr
       @priv_port = Port.new priv_port
       @pub_port = Port.new pub_port
       @protocol = Protocol.new protocol
+    end
+
+    def db_save(db)
+      db.save_nat_port self
     end
   end
 
@@ -104,12 +161,21 @@ module IPTWr
 
     end
 
-    # @param [PortRange] pr
-    def add_range(ip, pr)
-      proto = pr.proto
-      pr.each do |p|
+    def add_range(ip, proto, p1, p2)
+      raise 'Ports order mismatch' if p1 > p2
+      p1.step(p2,1) do |p|
         np = NatPort.new nil,ip,nil,p,proto
         @nat_ports.push np
+      end
+    end
+
+    def add_nat_port(np)
+      @nat_ports.push np
+    end
+
+    def db_save(db)
+      @nat_ports.each do |p|
+        p.db_save db
       end
     end
 
