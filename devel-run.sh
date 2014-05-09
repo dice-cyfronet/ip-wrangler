a9cc33a34b86d433731435abfde497bc8a4cf667
Paweł Suder <pawel@suder.info>
2014-05-09 09:19:45 +0200

    Update devel script

--- devel-run.sh
+++ devel-run.sh
@@ -3,4 +3,27 @@
 export __DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export __NO_LOG=1
 
-${__DIR}/run.sh $@
+export __PORT=8400
+export __IP=0.0.0.0
+export __TAG=IptWr
+
+while getopts 'i:p:t:' __FLAG; do
+  case "${__FLAG}" in
+    i)
+        export __IP=${OPTARG}
+        ;;
+    p)
+        export __PORT=${OPTARG}
+        ;;
+    t)
+        export __TAG=${OPTARG}
+        ;;
+    *)
+        error "Unexpected option ${__FLAG}"
+        ;;
+  esac
+done
+
+pushd ${__DIR}/src/
+thin -a ${__IP} -p ${__PORT} -R ./config.ru --tag ${__TAG} start
+popd
