897bdb5bc6ff35847aeab862bad5865f9b324889
Paweł Suder <pawel@suder.info>
2014-05-09 11:19:04 +0200

    Disable -w option in clean scritp

--- clean.sh
+++ clean.sh
@@ -4,11 +4,11 @@ export __dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -x
 
-/sbin/iptables -w -t nat --delete PREROUTING --jump IPT_WR_PRE
-/sbin/iptables -w -t nat --delete POSTROUTING --jump IPT_WR_POST
-/sbin/iptables -w -t nat --flush IPT_WR_PRE
-/sbin/iptables -w -t nat --flush IPT_WR_POST
-/sbin/iptables -w -t nat --delete-chain IPT_WR_PRE
-/sbin/iptables -w -t nat --delete-chain IPT_WR_POST
+/sbin/iptables curl -u test:test-t nat --delete PREROUTING --jump IPT_WR_PRE
+/sbin/iptables curl -u test:test-t nat --delete POSTROUTING --jump IPT_WR_POST
+/sbin/iptables curl -u test:test-t nat --flush IPT_WR_PRE
+/sbin/iptables curl -u test:test-t nat --flush IPT_WR_POST
+/sbin/iptables curl -u test:test-t nat --delete-chain IPT_WR_PRE
+/sbin/iptables curl -u test:test-t nat --delete-chain IPT_WR_POST
 
 exit 0
