5958f7c257044a7bbb0438b4534d972b8a0872b7
Paweł Suder <ymsuder@cyfronet.pl>
2014-08-20 12:39:24 +0200

    Update clean.sh, add sudo.

--- clean.sh
+++ clean.sh
@@ -4,11 +4,11 @@ export __dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -x
 
-/sbin/iptables -t nat --delete PREROUTING --jump IPT_WR_PRE
-/sbin/iptables -t nat --delete POSTROUTING --jump IPT_WR_POST
-/sbin/iptables -t nat --flush IPT_WR_PRE
-/sbin/iptables -t nat --flush IPT_WR_POST
-/sbin/iptables -t nat --delete-chain IPT_WR_PRE
-/sbin/iptables -t nat --delete-chain IPT_WR_POST
+sudo /sbin/iptables -t nat --delete PREROUTING --jump IPT_WR_PRE
+sudo /sbin/iptables -t nat --delete POSTROUTING --jump IPT_WR_POST
+sudo /sbin/iptables -t nat --flush IPT_WR_PRE
+sudo /sbin/iptables -t nat --flush IPT_WR_POST
+sudo /sbin/iptables -t nat --delete-chain IPT_WR_PRE
+sudo /sbin/iptables -t nat --delete-chain IPT_WR_POST
 
 exit 0
